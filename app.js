1437065390572cf4ed67b1ade6cf8bfb6a05300e
jeet <jeetbanik332004@gmail.com>
2024-08-21 01:05:43 +0530

    create and show route

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ async function main() {
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("welcome to root");
 });
@@ -36,6 +37,23 @@ app.get("/", (req, res) => {
 app.get("/listings", async (req, res) => {
   let allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
+});
+//new route:
+app.get("/listings/new", (req, res)=>{
+  res.render("listings/new.ejs");
+});
+//show route:
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+//create route:
+app.post("/listings", async (req, res) => {
+  // let listing = req.body.listing;
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 app.listen(8080, () => {
   console.log("server is listening at port 8080");
