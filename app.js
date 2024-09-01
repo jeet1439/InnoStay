3b7f57fbb107db6dea604376dd8cc98e00cbc9c6
jeet <jeetbanik332004@gmail.com>
2024-09-01 23:56:29 +0530

    review_model

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const  { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 main()
   .then(() => {
     console.log("connected to DB");
@@ -93,6 +94,17 @@ app.delete("/listings/:id", wrapAsync( async (req, res) =>{
   console.log(deletedListing);
   res.redirect("/listings");
 }));
+
+//reviews: post route
+app.post("/listings/:id/reviews", async(req, res) => {
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  console.log("review saved");
+  res.send("saved");
+});
 
 app.all("*", (req, res, next) =>{
   next(new ExpressError(404, "page not found"));
